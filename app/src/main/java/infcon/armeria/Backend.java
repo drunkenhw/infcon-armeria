4367367701d76c9da417f62f7df290fec61aff19
drunkenhw <drunkenhw@gmail.com>
2022-08-26 23:13:47 +0900

    infcon

--- app/src/main/java/infcon/armeria/Backend.java
+++ app/src/main/java/infcon/armeria/Backend.java
@@ -1,5 +1,28 @@
 package infcon.armeria;
 
+import java.time.Duration;
+
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.Server;
+
 public final class Backend {
 
+    public static Backend of(String name, int port){
+        return new Backend(name, port);
+    }
+
+    private final Server server;
+
+    private Backend(String name, int port) {
+        server = Server.builder()
+              .http(port)
+              .service('/'+ name,(ctx, req) -> {
+                  final HttpResponse res = HttpResponse.of("response from : " + name);
+                  return HttpResponse.delayed(res, Duration.ofSeconds(3));
+               })
+                .build();
+    }
+    public void start(){
+        server.start().join();
+    }
 }
--- app/src/test/java/infcon/armeria/BackendTest.java
+++ app/src/test/java/infcon/armeria/BackendTest.java
@@ -1,11 +1,38 @@
 package infcon.armeria;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.junit.jupiter.api.Test;
+
+import com.linecorp.armeria.client.WebClient;
+import com.linecorp.armeria.common.AggregatedHttpResponse;
+import com.linecorp.armeria.common.HttpResponse;
 
 class BackendTest {
 
     @Test
-    void backend() {
+    void backend() throws InterruptedException {
+        final Backend foo = Backend.of("foo", 9000);
+        foo.start();
 
+        final WebClient webClient = WebClient.of("http://127.0.0.1:9000");
+
+        //3초 뒤에 리턴해도 비동기서버이기에 응답을 갖고있지 않다.
+        final HttpResponse httpResponse = webClient.get("/foo");
+        final CompletableFuture<AggregatedHttpResponse> future = httpResponse.aggregate();
+        //call back
+        future.thenAccept(aggregatedHttpResponse -> {
+            //event loop 가 받음.
+            System.err.println("In callback. Thread Name: "+ Thread.currentThread().getName());
+            sendBackToTheOriginalClient(aggregatedHttpResponse);
+        });
+
+        Thread.sleep(Long.MAX_VALUE);
+
+
+
+    }
+
+    private void sendBackToTheOriginalClient(AggregatedHttpResponse aggregatedHttpResponse) {
     }
 }
